3242ffcb387094f975b6931796d7f8a5d5e6aac4
relo-ga <40379438+relo-ga@users.noreply.github.com>
2024-12-17 03:56:19 +0000

    Random Card Generator V.1

--- src/app.js
+++ src/app.js
@@ -7,5 +7,45 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const pintaArriba = document.querySelector("#pintaArriba");
+  const pintaAbajo = document.querySelector("#pintaAbajo");
+  const numero = document.querySelector("#numero");
+
+  const pintas = ["♦", "♥", "♠", "♣"];
+  const valores = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  function generarNumeroAl(lista) {
+    let numeroAleatorio = Math.floor(Math.random() * lista.length);
+
+    return lista[numeroAleatorio];
+  }
+
+  let randomPintas = generarNumeroAl(pintas);
+  let randomNumero = generarNumeroAl(valores);
+
+  pintaArriba.innerHTML = randomPintas;
+  pintaAbajo.innerHTML = randomPintas;
+  numero.innerHTML = randomNumero;
+
+  pintaAbajo.style.transform = "rotate(180deg)";
+
+  if (randomPintas == "♦" || randomPintas == "♥") {
+    pintaArriba.style.color = "red";
+    pintaAbajo.style.color = "red";
+    numero.style.color = "red";
+  }
 };
